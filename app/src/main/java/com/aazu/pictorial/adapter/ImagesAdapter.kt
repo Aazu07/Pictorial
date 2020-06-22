d05b673707ab0bf7be6d72cef20db540cd0577fa
Aastha Gupta <aastha.gupta@railyatri.in>
2020-06-22 13:01:49 +0530

    Fixes

--- app/src/main/java/com/aazu/pictorial/adapter/ImagesAdapter.kt
+++ app/src/main/java/com/aazu/pictorial/adapter/ImagesAdapter.kt
@@ -2,10 +2,11 @@ package com.aazu.pictorial.adapter
 
 import android.content.Context
 import android.util.DisplayMetrics
+import android.util.Log
+import android.util.Size
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.aazu.pictorial.MainActivity
@@ -50,26 +51,30 @@ class ImagesAdapter(private val context: Context, private val list: ArrayList<Im
         RecyclerView.ViewHolder(itemBinding.root) {
 
         fun onBind() {
-//            val size = getSize(list[adapterPosition])
-            itemBinding.image.layoutParams = ConstraintLayout.LayoutParams(
-                (screenWidth / 2),
-                ConstraintLayout.LayoutParams.WRAP_CONTENT
-            )
+            val size: Size
+            if (list[adapterPosition].width != 0 && list[adapterPosition].height != 0) {
+                size = getSize(list[adapterPosition])
+            } else {
+                size = Size((screenWidth / 2), 0)
+            }
             itemBinding.path = list[adapterPosition].uri.toString()
+            itemBinding.width = size.width
+            itemBinding.height = size.height
         }
 
-//        Unable to use this approach as webP images have 0 width and height
-//        private fun getSize(imageFromStorage: ImageFromStorage) : Size {
-//
-//            Log.d("ORIGINAL", "original width ${imageFromStorage.width} height ${imageFromStorage.height}")
-//
-//            val aspectRatio = imageFromStorage.width.toFloat() / imageFromStorage.height.toFloat()
-//            Log.d("ASPECTRATIO", "aspectratio $aspectRatio")
-//            val newWidth = screenWidth /2
-//            val newHeight =  newWidth.toFloat() / aspectRatio
-//            Log.d("SIZES", "Width $newWidth and height $newHeight")
-//            return Size(newWidth, newHeight.toInt())
-//        }
+        //        Unable to use this approach as webP images have 0 width and height
+        private fun getSize(imageFromStorage: ImageFromStorage): Size {
+            Log.d(
+                "ORIGINAL",
+                "original width ${imageFromStorage.width} height ${imageFromStorage.height}"
+            )
+            val aspectRatio = imageFromStorage.width.toFloat() / imageFromStorage.height.toFloat()
+            Log.d("ASPECTRATIO", "aspectratio $aspectRatio")
+            val newWidth = screenWidth / 2
+            val newHeight = newWidth.toFloat() / aspectRatio
+            Log.d("SIZES", "Width $newWidth and height $newHeight")
+            return Size(newWidth, newHeight.toInt())
+        }
     }
 
 }
--- app/src/main/java/com/aazu/pictorial/utils/CommonBindingUtils.kt
+++ app/src/main/java/com/aazu/pictorial/utils/CommonBindingUtils.kt
@@ -4,15 +4,19 @@ import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 object CommonBindingUtils {
 
     @JvmStatic
-    @BindingAdapter("imageUri")
-    fun setImageFromPath(imageview: ImageView, imageURI: String) {
-        Glide.with(imageview.context).load(Uri.parse(imageURI))
-            .into(imageview)
-//            .apply(RequestOptions().override(100, 100))
+    @BindingAdapter(value = ["imageUri", "width", "height"], requireAll = true)
+    fun setImageFromPath(imageview: ImageView, imageURI: String?, width: Int, height: Int) {
+        if (imageURI != null) {
+            Glide.with(imageview.context).load(Uri.parse(imageURI))
+                .apply(RequestOptions().override(width, height))
+                .into(imageview)
+
+        }
 
     }
 }
